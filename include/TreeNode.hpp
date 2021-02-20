ce82b4daebdf5622b25d85d0e71718038ab68f2f
Andrey Dergach <thequasar7@gmail.com>
2021-02-20 12:54:19 +0200

    Implemented TreeNode class

---
+++ include/TreeNode.hpp
@@ -0,0 +1,125 @@
+class TreeNode
+{
+    /**
+     * If leftChild & rightChild == nullptr --> this == Leaf
+     * If this == Leaf & weight == 0 --> this == NYT
+     */
+
+private:
+    int weight;
+    int nodeId;
+    char symbol;
+    TreeNode *leftChild;
+    TreeNode *rightChild;
+    TreeNode *parentNode;
+
+public:
+    TreeNode(int id, TreeNode *left, TreeNode *right);
+    TreeNode(int id, char s);
+
+    int getWeight();
+    void setWeight(int w);
+    int getNodeId();
+    void setNodeId(int id);
+    char getSymbol();
+    void setSymbol(char s);
+    TreeNode *getLeftChild();
+    void setLeftChild(TreeNode *child);
+    TreeNode *getRightChild();
+    void setRightChild(TreeNode *child);
+    TreeNode *getParent();
+    void setParent(TreeNode *parent);
+
+    void incWeight();
+};
+
+TreeNode::TreeNode(int id, TreeNode *left, TreeNode *right)
+{
+    nodeId = id;
+    symbol = 0;
+    leftChild = left;
+    rightChild = right;
+    weight = left->getWeight() + right->getWeight();
+}
+
+TreeNode::TreeNode(int node_id, char symb)
+{
+    nodeId = node_id;
+    symbol = symb;
+    leftChild = nullptr;
+    rightChild = nullptr;
+
+    if (symb)
+    {
+        weight = 1;
+    }
+    else
+    {
+        weight = 0;
+    }
+}
+
+int TreeNode::getWeight()
+{
+    return weight;
+}
+
+void TreeNode::setWeight(int w)
+{
+    weight = w;
+}
+
+int TreeNode::getNodeId()
+{
+    return nodeId;
+}
+
+void TreeNode::setNodeId(int id)
+{
+    nodeId = id;
+}
+
+char TreeNode::getSymbol()
+{
+    return symbol;
+}
+
+void TreeNode::setSymbol(char s)
+{
+    symbol = s;
+}
+
+TreeNode *TreeNode::getLeftChild()
+{
+    return leftChild;
+}
+
+void TreeNode::setLeftChild(TreeNode *child)
+{
+    leftChild = child;
+}
+
+TreeNode *TreeNode::getRightChild()
+{
+    return rightChild;
+}
+
+void TreeNode::setRightChild(TreeNode *child)
+{
+    rightChild = child;
+}
+
+TreeNode *TreeNode::getParent()
+{
+    return parentNode;
+}
+
+void TreeNode::setParent(TreeNode *parent)
+{
+    parentNode = parent;
+}
+
+void TreeNode::incWeight()
+{
+    weight++;
+}
